fadc2210396921dfc85b653fa1886555c667b784
Revati <revati.v.bordawekar@gmail.com>
2022-12-21 10:30:39 -0800

    added user reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+import userReducer from './reducers/user'
+
+const store = configureStore({
+  reducer: {
+    user: userReducer
+  }
+})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
